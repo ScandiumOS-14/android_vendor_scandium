36db770f6f4fca74680893acba5ddea4ca5ca5e5
PGYT PROJECT <155278452+KernelBuilding@users.noreply.github.com>
2025-06-26 18:01:51 +0700

    tools: update ota json format

--- tools/generate_json_build_info.sh
+++ tools/generate_json_build_info.sh
@@ -19,19 +19,23 @@ if [ "$1" ]; then
             datetime=$(grep ro\.build\.date\.utc ./out/target/product/$DEVICE/system/build.prop | cut -d= -f2);
             id=$(cat "$file_path.sha256sum" | cut -d' ' -f1);
             build_type=$(grep ro\.scandium\.buildtype ./out/target/product/$DEVICE/system/build.prop | cut -d= -f2);
+            maintainer=$(grep ro\.scandium\.maintainer ./out/target/product/$DEVICE/system/build.prop | cut -d= -f2);
+            device=$(grep ro\.scandium\.device ./out/target/product/$DEVICE/system/build.prop | cut -d= -f2);
             base_version=$(grep org\.scandium\.version ./out/target/product/$DEVICE/system/build.prop | cut -d= -f2);
             link="https://sourceforge.net/projects/scandiumos/files/${DEVICE}/${file_name}/download"
             echo "{" > $file_path.json
             echo "  \"response\": [" >> $file_path.json
             echo "    {" >> $file_path.json
-            echo "      \"datetime\": ${datetime}," >> $file_path.json
-            echo "      \"size\": ${file_size}," >> $file_path.json
-            echo "      \"filehash\": \"${md5}\"," >> $file_path.json
+            echo "      \"maintainer\": \"$maintainer\"," >> $file_path.json
+            echo "      \"device\": \"$device\"," >> $file_path.json
+            echo "      \"datetime\": \"${datetime}\"," >> $file_path.json
+            echo "      \"size\": \"${file_size}\"," >> $file_path.json
+            echo "      \"md5\": \"${md5}\"," >> $file_path.json
             echo "      \"filename\": \"${file_name}\"," >> $file_path.json
-            echo "      \"id\": \"${id}\"," >> $file_path.json
-            echo "      \"romtype\": \"${build_type}\"," >> $file_path.json
+            echo "      \"sha256\": \"${id}\"," >> $file_path.json
+            echo "      \"buildtype\": \"${build_type}\"," >> $file_path.json
             echo "      \"version\": \"${base_version}\"," >> $file_path.json
-            echo "      \"url\": \"${link}\"" >> $file_path.json
+            echo "      \"download\": \"${link}\"" >> $file_path.json
             echo "    }" >> $file_path.json
             echo "  ]" >> $file_path.json
             echo "}" >> $file_path.json
